5923cdf6ee72a6fc177de0eacefbcabea0ed9142
Purice Victor <pvictor0110@gmail.com>
2023-10-02 16:56:19 +0300

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,18 +12,27 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react'
 import { ReactNode } from 'react'
+import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa'
+import { BiMailSend } from 'react-icons/bi'
 
 const Footer = () => {
   return (
-    <Box>
-      <Container>
-        <SimpleGrid>
-          <Stack>
+    <Box
+    bg={useColorModeValue('gray.50', 'gray.900')}
+    color={useColorModeValue('gray.700', 'gray.200')}
+    >
+      <Container as={Stack} maxW={'6xl'} py={10}>
+        <SimpleGrid 
+           templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 2fr' }}
+           spacing={8}
+        >
+          <Stack spacing={6}>
             <Box>
               <Text>Logo</Text>
             </Box>
-            <Text>© 2023 Company Name. All rights reserved</Text>
-            <Stack>
+
+            <Text fontSize={'sm'}>© 2023 Company Name. All rights reserved</Text>
+            <Stack direction={'row'} spacing={6}>
               <Text>
                 Twitter
               </Text>
@@ -35,7 +44,7 @@ const Footer = () => {
               </Text>
             </Stack>
           </Stack>
-          <Stack>
+          <Stack align={'flex-start'}>
             <Text>
               Should import here list header
             </Text>
@@ -43,20 +52,35 @@ const Footer = () => {
               About us
             </Box>
             <Box>
-              About us
+              Blog
             </Box>
             <Box>
-              About us
+              Pricing
             </Box>
             <Box>
-              About us
+              Testimonials
             </Box>
           </Stack>
-          <Stack>
+          <Stack align={'flex-start'}>
             <Text> Should import here list header</Text>
-            <Stack>
-              <Input/>
-              <IconButton/>
+            <Stack direction={'row'}>
+              <Input
+                placeholder='Your email address'
+                bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+                border={0}
+                _focus={{
+                  bg: 'whiteAlpha.300',
+                }}
+              />
+              <IconButton
+                bg={useColorModeValue('green.400', 'green.800')}
+                color={useColorModeValue('white', 'gray.800')}
+                _hover={{
+                  bg: 'green.600',
+                }}
+                aria-label="Subscribe"
+                icon={<BiMailSend />}
+              />
             </Stack>
           </Stack>
         </SimpleGrid>
