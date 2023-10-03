80ff57233d52b5bfe0c8dcf3f99c59f4e2dc73a5
Purice Victor <pvictor0110@gmail.com>
2023-10-03 17:41:51 +0300

    created faq and contact form

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,10 +1,122 @@
 import React from 'react'
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  Flex,
+  useColorModeValue,
+  Text,
+  Container,
+  Heading, 
+  Stack,
+} from '@chakra-ui/react'
+
+import { ChevronDownIcon } from '@chakra-ui/icons'
 
 const Faq = () => {
   return (
-    <div>
-      Frecvent Asked Questions
-    </div>
+    <Flex
+    minH={'100vh'}
+    align={'center'}
+    justify={'center'}
+    flexDirection={'column'}
+    bg={useColorModeValue('gray.50', 'gray.800')}>
+
+    <Stack spacing={4} as={Container} maxW={'3xl'} textAlign={'center'}>
+      <Heading fontSize={{ base: '2xl', sm: '4xl' }} fontWeight={'bold'}>
+        What we offer
+      </Heading>
+      <Text color={'gray.600'} fontSize={{ base: 'sm', sm: 'lg' }}>
+      Discover the best deals you'll ever find with our unbeatable offers. We're dedicated to providing you with the best value for your money, so you can enjoy top-quality services and products without breaking the bank. Our deals are designed to give you the ultimate renting experience, so don't miss out on your chance to save big.
+      </Text>
+    </Stack>
+
+    <Container>
+      <Accordion allowMultiple width="100%" maxW="lg" rounded="lg">
+        <AccordionItem>
+          <AccordionButton
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            p={4}>
+            <Text fontSize="md">How old do I need to be to rent a car ?</Text>
+            <ChevronDownIcon fontSize="24px" />
+          </AccordionButton>
+          <AccordionPanel pb={4}>
+            <Text color="gray.600">
+            You typically need to be at least 21 years old to rent a car. Some rental companies may have a minimum age requirement of 25.
+            </Text>
+          </AccordionPanel>
+        </AccordionItem>
+
+        <AccordionItem>
+          <AccordionButton
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            p={4}>
+            <Text fontSize="md">What documents do I need to rent a car?</Text>
+            <ChevronDownIcon fontSize="24px" />
+          </AccordionButton>
+          <AccordionPanel pb={4}>
+            <Text color="gray.600">
+              You will typically need a valid driver's license, a major credit card in your name, and sometimes an additional form of identification like a passport.
+            </Text>
+          </AccordionPanel>
+        </AccordionItem>
+
+        <AccordionItem>
+          <AccordionButton
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            p={4}>
+            <Text fontSize="md"> Do I need insurance when renting a car?</Text>
+            <ChevronDownIcon fontSize="24px" />
+          </AccordionButton>
+          <AccordionPanel pb={4}>
+            <Text color="gray.600">
+              Most rental companies offer optional insurance coverage, but it's advisable to check if your existing auto insurance or credit card offers rental car coverage. This can help you avoid purchasing unnecessary insurance.
+            </Text>
+          </AccordionPanel>
+        </AccordionItem>
+
+        <AccordionItem>
+          <AccordionButton
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            p={4}>
+            <Text fontSize="md">What should I do in case of an accident or breakdown?</Text>
+            <ChevronDownIcon fontSize="24px" />
+          </AccordionButton>
+          <AccordionPanel pb={4}>
+            <Text color="gray.600">
+              In the event of an accident, contact local authorities and the rental company immediately. They will provide guidance on how to proceed. Most rental cars come with roadside assistance for breakdowns.
+            </Text>
+          </AccordionPanel>
+        </AccordionItem>
+
+        <AccordionItem>
+          <AccordionButton
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            p={4}>
+            <Text fontSize="md">What should I do in case of an accident or breakdown?</Text>
+            <ChevronDownIcon fontSize="24px" />
+          </AccordionButton>
+          <AccordionPanel pb={4}>
+            <Text color="gray.600">
+              In the event of an accident, contact local authorities and the rental company immediately. They will provide guidance on how to proceed. Most rental cars come with roadside assistance for breakdowns.
+            </Text>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+    </Container>
+  </Flex>
+
   )
 }
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,170 @@
 import React from 'react'
+import {
+  Container,
+  Flex,
+  Box,
+  Heading,
+  Text,
+  IconButton,
+  Button,
+  VStack,
+  HStack,
+  Wrap,
+  WrapItem,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Textarea,
+} from '@chakra-ui/react'
+import {
+  MdPhone,
+  MdEmail,
+  MdLocationOn,
+  MdFacebook,
+  MdOutlineEmail,
+} from 'react-icons/md'
+import { BsWhatsapp, BsPerson, BsInstagram } from 'react-icons/bs'
+import {LiaFacebook} from 'react-icons/lia';
 
 const Contact = () => {
   return (
-    <div>
-      Contact
-    </div>
+    <>
+      <Container  maxW="full" mt={0} centerContent overflow="hidden">
+      <div>contact adress</div>
+      <Flex align={'baseline'}>
+        <div>need additional info?</div>
+
+        <Box
+          bg="white"
+          color="black"
+          borderRadius="lg"
+          border={'4px solid red'}
+          m={{ sm: 4, md: 16, lg: 10 }}
+          p={{ sm: 5, md: 5, lg: 16 }}>
+
+            <Box p={4}>
+            <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
+              <WrapItem>
+                <Box>
+                  <Heading>Contact</Heading>
+                  <Text mt={{ sm: 3, md: 3, lg: 5 }} color="black">
+                    Fill up the form below to contact
+                  </Text>
+                  <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
+                    <VStack pl={0} spacing={3} alignItems="flex-start">
+                      <Button
+                        size="md"
+                        height="48px"
+                        width="200px"
+                        variant="ghost"
+                        color=''
+                        _hover={{ border: '2px solid red' }}
+                        leftIcon={<MdPhone color="red" size="20px" />}>
+                        +91-988888888
+                      </Button>
+                      <Button
+                        size="md"
+                        height="48px"
+                        width="200px"
+                        variant="ghost"
+                        color=""
+                        _hover={{ border: '2px solid red' }}
+                        leftIcon={<MdEmail color="red" size="20px" />}>
+                        ondrive@mail.ro
+                      </Button>
+                      <Button
+                        size="md"
+                        height="48px"
+                        width="200px"
+                        variant="ghost"
+                        color=""
+                        _hover={{ border: '2px solid red' }}
+                        leftIcon={<MdLocationOn color="red" size="20px" />}>
+                        Sibiu, Romania
+                      </Button>
+                    </VStack>
+                  </Box>
+                  <HStack
+                    mt={{ lg: 10, md: 10 }}
+                    spacing={5}
+                    px={5}
+                    alignItems="flex-start">
+                    <IconButton
+                      aria-label="facebook"
+                      variant="ghost"
+                      size="lg"
+                      isRound={true}
+                      _hover={{ bg: 'red' }}
+                      icon={<LiaFacebook size="40px" />}
+                    />
+                    <IconButton
+                      aria-label="whatsapp"
+                      variant="ghost"
+                      size="lg"
+                      isRound={true}
+                      _hover={{ bg: 'red' }}
+                      icon={<BsWhatsapp size="28px" />}
+                    />
+                    <IconButton
+                      aria-label="instagram"
+                      variant="ghost"
+                      size="lg"
+                      isRound={true}
+                      _hover={{ bg: 'red' }}
+                      icon={<BsInstagram size="28px" />}
+                    />
+                  </HStack>
+                </Box>
+              </WrapItem>
+              <WrapItem>
+                <Box bg="white" borderRadius="lg">
+                  <Box m={8} color="">
+                    <VStack spacing={5}>
+                      <FormControl id="name">
+                        <FormLabel>Your Name</FormLabel>
+                        <InputGroup borderColor="#E0E1E7">
+                          <InputLeftElement pointerEvents="none">
+                            <BsPerson color="gray.800" />
+                          </InputLeftElement>
+                          <Input type="text" size="md" />
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl id="name">
+                        <FormLabel>Mail</FormLabel>
+                        <InputGroup borderColor="#E0E1E7">
+                          <InputLeftElement pointerEvents="none">
+                            <MdOutlineEmail color="gray.800" />
+                          </InputLeftElement>
+                          <Input type="text" size="md" />
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl id="name">
+                        <FormLabel>Message</FormLabel>
+                        <Textarea
+                          borderColor="gray.300"
+                          _hover={{
+                            borderRadius: 'gray.300',
+                          }}
+                          placeholder="message"
+                        />
+                      </FormControl>
+                      <FormControl id="name" float="right">
+                        <Button variant="solid" bg="red" color="white" _hover={{}}>
+                          Send Message
+                        </Button>
+                      </FormControl>
+                    </VStack>
+                  </Box>
+                </Box>
+              </WrapItem>
+            </Wrap>
+          </Box>
+        </Box>
+      </Flex>
+    </Container>
+    </>
   )
 }
 
