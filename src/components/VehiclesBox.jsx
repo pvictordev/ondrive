7513b9739a22ac3815d790307d3bb851f7c43bda
Purice Victor <pvictor0110@gmail.com>
2023-10-04 22:28:03 +0300

    commit

--- src/components/VehiclesBox.jsx
+++ src/components/VehiclesBox.jsx
@@ -1,17 +1,32 @@
 import React, {useState} from 'react'
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Text,
+  Stack,
+} from "@chakra-ui/react";
 
 const VehiclesBox = (props) => {
   
   return (
-    <div>
+    <Stack align={'center'} direction={'row'} >
       <div>     
         {props.name}
       </div>
       <div>     
+        {props.model}
+        {props.mark}
+        {props.year}
+        {props.doors}
+        {props.transmission}
+        {props.fuel}
         ${props.price}
+        {props.year}
       </div>
 
-    </div>
+    </Stack>
   )
 }
 
