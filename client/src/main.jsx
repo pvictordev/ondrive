2aab0f0c8aeb54c99fe3332dc46dc7aa18aaa41e
Victor Purice <victor.purice@ulbsibiu.ro>
2024-03-09 18:34:00 +0200

    main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
