e474d9fb028c2357177215fc4928ef3924460c32
Victor Purice <victor.purice@ulbsibiu.ro>
2024-03-09 18:32:47 +0200

    fixed some details

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,30 +1,28 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import BookRide from '../components/BookRide'
-import RentSteps from '../components/RentSteps'
-import Vehicles from '../components/Vehicles'
-import Offer from '../components/Offer'
-import Testimonials from '../components/Testimonials'
-import Banner from '../components/Banner'
-import Faq from '../components/Faq' 
+import Hero from "../components/Hero";
+import BookRide from "../components/BookRide";
+import RentSteps from "../components/RentSteps";
+import Vehicles from "../components/Vehicles";
+import Offer from "../components/Offer";
+import Testimonials from "../components/Testimonials";
+import Banner from "../components/Banner";
+import Faq from "../components/Faq";
 
 const Home = () => {
   return (
-  <>
-    <Hero/>
-    <BookRide/>
-    <RentSteps/>
-    <Vehicles/>
-    <Banner title='Save your cash and never crash !' textColor="Better" description='than competitors, we have things that they do not'/>
-    <Offer/>
-    <Testimonials/>
-    <Faq/>
-  </>
-  )
-}
-export default Home
-
-
-
-
-
+    <>
+      <Hero />
+      <BookRide />
+      <RentSteps />
+      <Vehicles />
+      <Banner
+        title="Save your cash and never crash !"
+        textColor="Better"
+        description="than competitors, we have things that they do not"
+      />
+      <Offer />
+      <Testimonials />
+      <Faq />
+    </>
+  );
+};
+export default Home;
--- client/src/pages/Team.jsx
+++ client/src/pages/Team.jsx
@@ -1,40 +1,43 @@
-import React from 'react'
-import TeamMember from '../Data/TeamMember'
-import TeamCard from '../components/TeamCard'
-import { Grid, useMediaQuery, Box  } from '@chakra-ui/react'
-import HomeRoute from '../components/HomeRoute'
-import Banner from '../components/Banner'
-import RentSteps from '../components/RentSteps'
+import TeamMember from "../Data/TeamMember";
+import TeamCard from "../components/TeamCard";
+import { Grid, Box } from "@chakra-ui/react";
+import HomeRoute from "../components/HomeRoute";
+import Banner from "../components/Banner";
+import RentSteps from "../components/RentSteps";
 
 const Team = () => {
-  const cards = TeamMember.map(member => {
-    return(
-      <TeamCard
-        className='Team__card'
-        key={member.id}
-        {...member}
-      />
-    )
-  })
+  const cards = TeamMember.map((member) => {
+    return <TeamCard className="Team__card" key={member.id} {...member} />;
+  });
   return (
-    <Box className='Team'>
-      <HomeRoute className="Team__title" title={'Team'}/>
+    <Box className="Team">
+      <HomeRoute className="Team__title" title={"Team"} />
       <Grid
-        className='Team__container' 
-       templateColumns={{ base: "1fr", lg: "repeat(2, 1fr)", xl: "repeat(3, 1fr)" }}
-       templateRows={{ base: "repeat(6, 1fr)", lg: "repeat(3, 1fr)", xl: "repeat(2, 1fr)" }}
+        className="Team__container"
+        templateColumns={{
+          base: "1fr",
+          lg: "repeat(2, 1fr)",
+          xl: "repeat(3, 1fr)",
+        }}
+        templateRows={{
+          base: "repeat(6, 1fr)",
+          lg: "repeat(3, 1fr)",
+          xl: "repeat(2, 1fr)",
+        }}
       >
         {cards}
       </Grid>
 
       <Banner
         className="Team__banner"
-        title='Meet Our Team !' textColor="We" description='are the best in the business '
-        /> 
+        title="Meet Our Team !"
+        textColor="We"
+        description="are the best in the business "
+      />
 
-      <RentSteps className='Team__steps'/>
+      <RentSteps className="Team__steps" />
     </Box>
-  )
-}
+  );
+};
 
-export default Team
+export default Team;
